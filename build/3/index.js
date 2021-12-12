ca80d170eddb47407f268bc0d2ebd9bdb3800591
analuizasb2 <analuizasb2@gmail.com>
2021-12-12 18:23:38 -0300

    removing build folder

--- build/3/index.js
+++
@@ -1,6 +0,0 @@
-import { stringArrayFromFile } from "../utils/FileReader.js";
-import { BinaryDiagnostic } from "./BinaryDiagnostic.js";
-var inputBinaryDiagnostic = stringArrayFromFile("../inputs/3.txt");
-export var printSolutionBinaryDiagnostic = function () {
-    console.log("Day 3: Binary Diagnostic, 1st:", BinaryDiagnostic.calculatePowerConsumptionDecimal(inputBinaryDiagnostic));
-};
--- build/index.js
+++
@@ -1,6 +0,0 @@
-import { printSolutionSonarSweep } from "./1/index.js";
-import { printSolutionDive } from "./2/index.js";
-import { printSolutionBinaryDiagnostic } from "./3/index.js";
-printSolutionSonarSweep();
-printSolutionDive();
-printSolutionBinaryDiagnostic();
